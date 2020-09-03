e350449f59c81b97714e3e6d74d52d57ae42972f
GokoshiJr <julioagonzalez18@gmail.com>
2020-09-03 16:17:05 -0400

    Tipos Literales
    Son un tipo especial que en vez de registrar un rango de valores
    pertenecientes a un tipo primitivo o complejo(como un string, number, o
    un objeto de tres calve-valores)
    
    representa un unico valor primitivo, con el que le decimos a ys que una
    variable solo puede tener un unico valor de todos los valores posibles
    del mundo

---
+++ src/tipos/tiposLiterales.ts
@@ -0,0 +1,19 @@
+let verdadera: true = true;
+let cinco: 5 = 5;
+let hola: 'hola' = 'hola';
+
+let permiso: boolean = true;
+const bandera = true;
+const saludo = 'saludo';
+
+type LoginOperation = {
+  op: 'Login';
+  username: string;
+  password: string;
+};
+
+let l: LoginOperation = {
+  username: 'admin',
+  password: 'admin',
+  op: 'Login',
+};
