432a0fcff0ebbc58a005c938612dd2467977bbd4
GokoshiJr <julioagonzalez18@gmail.com>
2020-09-04 11:40:14 -0400

    Uniones de Tipos
    Con las uniones de tipos, ts nos permite crear tipos compuestos, para
    que su sistema de comprobaciones admita a multiples tipos de forma
    paralela
    
    Ej una funcion en la que quieres que acepte un numero como un string

---
+++ src/tipos/unionTipos.ts
@@ -0,0 +1,23 @@
+type A = {
+  uno: boolean;
+  dos: boolean;
+};
+
+type B = {
+  tres: boolean;
+};
+
+let value: A | B = {uno: true, dos: true};
+let value1: A | B = {tres: false};
+
+function convertir(valor: string | number) {
+  if (typeof valor === 'string') {
+    valor.toLocaleLowerCase();
+  } else if (typeof valor === 'number') {
+    console.log(valor);
+  }
+}
+
+convertir('asdf');
+convertir(5);
+convertir(true);
