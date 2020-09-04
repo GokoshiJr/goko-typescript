a82f0411fc5bae2f4181a766cd8daac5b9baa691
GokoshiJr <julioagonzalez18@gmail.com>
2020-09-04 13:25:27 -0400

    Interfaces - Modificadores

--- src/interfaces/interfaz.ts
+++ src/interfaces/interfaz.ts
@@ -1,16 +1,45 @@
-// introduccion
+// modificadores y funciones
+
 interface UserData {
-  userName: string;
-  createdDate: Date;
-  superUser: boolean;
+  readonly userName: string;
+  createdDate: Date | undefined;
+  superUser?: boolean; // superuser puede estar o no con el ?
+  readonly personal: {
+    firstName: string;
+    email: string;
+  };
+  logOut(): void;
+  rename: (userName: String) => void;
 }
 
 function loginUser(): UserData {
   return {
     userName: 'admin',
     createdDate: new Date(),
-    superUser: true,
+    // superUser: true,
+    logOut() {
+      console.log('Adios');
+    },
+    rename(userName) {
+      console.log('Todo: Rename account');
+    },
+    personal: {
+      firstName: 'Admin',
+      email: 'email@gmail.com',
+    },
   };
 }
 
 let data: UserData = loginUser();
+
+/* 
+if (data.createdDate) {
+  data.createdDate.toISOString();
+} 
+*/
+
+// forma segura, si es undefined no lo ejecuta ?.
+data.createdDate?.toISOString();
+
+// lo usamos si estamos seguros de que va a existir !.
+data.createdDate!.getDate();
